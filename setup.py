6f04ac7583542f19b011996aa105424958f2b598
accraze <accraze@gmail.com>
2016-08-27 12:05:05 -0700

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name='twelve-tone',
-    version='0.1.0',
+    version='0.2.0',
     license='BSD',
     description='Twelve-tone matrix to generate dodecaphonic melodies',
     long_description='%s\n%s' % (
