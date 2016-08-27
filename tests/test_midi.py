7c3c6a696a602ef81335a66a656114770d9b50f8
accraze <accraze@gmail.com>
2016-08-27 11:09:03 -0700

    test(midi): fix MIDIFile init test

--- tests/test_midi.py
+++ tests/test_midi.py
@@ -10,9 +10,10 @@ class TestMIDIFile(unittest.TestCase):
     def test_init(self):
         m = MIDIFile()
         self.assertEquals(m.step_counter, 0)
-        self.assertEquals(m.tone_rows, 1)
-        m = MIDIFile(tone_rows=2)
-        self.assertEquals(m.tone_rows, 2)
+        m = MIDIFile(BPM=200)
+        self.assertEquals(m.BPM, 200)
+        m = MIDIFile(filename="test.mid")
+        self.assertEquals(m.filename, 'test.mid')
 
     def test_create(self):
         notes = [1,2,3,4,5,6,7,8,9,10,11]
@@ -21,7 +22,7 @@ class TestMIDIFile(unittest.TestCase):
         os.chdir(path)
         m = MIDIFile()
         m.create(notes)
-        self.assertTrue(path.exists(os.path.join(os.getcwd(),file_name)))
+        self.assertTrue(os.path.exists(os.path.join(os.getcwd(),file_name)))
         os.chdir(os.pardir)
         shutil.rmtree('tmp', ignore_errors=True)
 
