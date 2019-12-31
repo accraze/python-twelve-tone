a1d9d2e649c961e1359e27d4df9974173792e7cf
Andy Craze <accraze@gmail.com>
2019-12-31 09:08:41 -0800

    Bump version: 0.4.0 → 0.4.1

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = u'Twelve Tone'
 year = u'2018'
 author = u'Andy Craze'
 copyright = '{0}, {1}'.format(year, author)
-version = release = u'0.4.0'
+version = release = u'0.4.1'
 
 pygments_style = 'trac'
 templates_path = ['.']
